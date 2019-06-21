13bf38c534b1c5ce4a6512ed128dd42ada961e77
github username <github email address>
2019-06-21 15:34:36 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,39 @@
 # My Code here....
+
+def map_to_negativize(array)
+    array.map { |i| i*-1 }
+end
+
+def map_to_no_change(array)
+    array.map { |i| i }
+end
+
+def map_to_double(array)
+    array.map { |i| i*2 }
+end
+
+def map_to_square(array)
+    array.map { |i| i ** 2 }
+end
+
+def reduce_to_total(array, starting_point = 0)
+    array.reduce(starting_point, :+)
+end
+
+def reduce_to_all_true(array)
+    i = 0
+    while i < array.length do
+      return false if !array[i]
+      i += 1
+    end
+    return true
+  end
+
+def reduce_to_any_true(array)
+    i = 0
+    while i < array.length do
+      return true if !!array[i]
+      i += 1
+    end
+    return false
+end
