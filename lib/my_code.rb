76193c00fa7ce8fa4702e2876acf8377b84f54ee
github username <github email address>
2019-10-29 13:48:39 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,57 @@
-# My Code here....
+require'pry'
+def map_to_negativize(source_array)
+    new_array = []
+    i = -1
+    source_array.each do |x|
+    new_array << x * i
+    end 
+    new_array
+end 
+
+def map_to_no_change(source_array)
+    return source_array
+end 
+
+def map_to_double(source_array)
+    new_array = []
+    i = 2
+    source_array.each do |x| 
+    new_array << x * i
+    end 
+    new_array
+end 
+
+def map_to_square(source_array)
+    new_array = []
+    i = 2 
+    source_array.each do |x|
+        new_array << x ** i 
+    end 
+    new_array
+end 
+
+def reduce_to_total(source_array, starting_point = 0)
+    total = starting_point
+    source_array.each do |current_element|
+       total += current_element 
+    end 
+        return total 
+    end 
+
+def reduce_to_all_true(source_array)
+    source_array.each do |current_element|
+        if current_element == false
+            return false
+        end 
+    end 
+end 
+
+def reduce_to_any_true(source_array)
+    source_array.each do |current_element|
+        if current_element == true
+          return true
+        
+        end 
+    end 
+    return false
+end 
